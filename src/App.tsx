f1623c1899e21e6e2635d56c2aba65bc1b086319
Peter Kang <pxk7062@rit.edu>
2024-11-25 23:26:00 -0500

    Added the calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
+import GitHubCalendar from 'react-github-calendar';
 
 function App() {
+  const queryString = window.location.search;
 
+// Create a URLSearchParams object
+  const urlParams = new URLSearchParams(queryString);
+  const userNameToUse = urlParams == null ? "": urlParams.get('username').toString()
   return (
     <>
+      <GitHubCalendar username={userNameToUse}>
+      </GitHubCalendar>
     </>
   )
 }
