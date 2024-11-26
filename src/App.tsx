a0e3a021c3c96cbe6bf6c3c22b3248e4743d789a
Peter Kang <pxk7062@rit.edu>
2024-11-25 23:27:10 -0500

    Updated comments

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
 // Create a URLSearchParams object
   const urlParams = new URLSearchParams(queryString);
   const userNameToUse = urlParams == null ? "": urlParams.get('username').toString()
+  //ie localhost:5173/?username=Peter-Kang
   return (
     <>
       <GitHubCalendar username={userNameToUse}>
